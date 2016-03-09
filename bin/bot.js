6fd7bef11d38588db91733384b2e721aea7a44d1
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2016-03-09 13:51:02 +0100

    ajout bdd

--- bin/bot.js
+++ bin/bot.js
@@ -3,10 +3,12 @@
 var Cardibot = require('../lib/cardibot');
 
 var token = process.env.BOT_API_KEY;
+var dbPath = process.env.BOT_DB_PATH;
 var name = process.env.BOT_NAME;
 
 var cardibot = new Cardibot({
     token: token,
+    dbPath: dbPath,
     name: name
 });
 
